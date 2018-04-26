edf976872ec6c50cf91e246d66329fd8c57ca593
Nathan-12 <nathanlima.b@gmail.com>
2018-04-26 11:16:56 -0300

    Função para adicionar ação de copiar ao botão

--- 4_Latex/src/main/webapp/js/show-modal.js
+++ 4_Latex/src/main/webapp/js/show-modal.js
@@ -2,3 +2,22 @@ $('#bt').click(function(){
 	$('#modal').modal(options);
 });
 
+
+$(function(){
+    // Executa o evento click no button
+    $('button').click(function(){
+        // Seleciona o conteúdo do input
+        $('textarea').select();
+        // Copia o conteudo selecionado
+        var copiar = document.execCommand('copy');
+        // Verifica se foi copia e retona mensagem
+        if(copiar){
+            alert('Copiado');
+        }else {
+            alert('Erro ao copiar, seu navegador pode não ter suporte a essa função.');
+        };
+        // Cancela a execução do formulário
+        return false;
+    });
+});
+

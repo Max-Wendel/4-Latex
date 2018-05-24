af6920df2e33cb4499cc17b1de78e85c1a4a1704
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-05-24 09:07:38 -0300

    Esqueleto do Controller de ArqImagem.

---
+++ src/main/java/com/g4dev/controller/ArqImagemController.java
@@ -0,0 +1,14 @@
+package com.g4dev.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.g4dev.model.ArqImagemModel;
+
+@Controller
+public class ArqImagemController {
+
+	/*Falta definir pra que página será redirecionada*/
+	public String salvar(ArqImagemModel arqImagem) {
+		return "";
+	}
+}

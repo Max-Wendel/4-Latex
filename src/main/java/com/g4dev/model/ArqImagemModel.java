34a5e3aa6b0c02ce7f4db6f8c36625a2147e03c0
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-05-24 08:36:34 -0300

    Configuração do model de ArqImagem.

---
+++ src/main/java/com/g4dev/model/ArqImagemModel.java
@@ -0,0 +1,79 @@
+package com.g4dev.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="arq_imagem")
+public class ArqImagemModel {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String diretorio;
+	
+	@OneToMany
+	@JoinColumn(name = "id_template")
+	private TemplateModel template;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDiretorio() {
+		return diretorio;
+	}
+	public void setDiretorio(String diretorio) {
+		this.diretorio = diretorio;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((diretorio == null) ? 0 : diretorio.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArqImagemModel other = (ArqImagemModel) obj;
+		if (diretorio == null) {
+			if (other.diretorio != null)
+				return false;
+		} else if (!diretorio.equals(other.diretorio))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+}

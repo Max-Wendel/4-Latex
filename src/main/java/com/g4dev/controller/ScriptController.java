4736045ab3ce671b90cbee12a62f75a5f409bbad
Max Wendel <maxwendelffaraujo@gmail.com>
2018-06-05 12:39:48 -0300

    Criação do model, controller e service de Script.

--- src/main/java/com/g4dev/controller/ScriptController.java
+++ src/main/java/com/g4dev/controller/ScriptController.java
@@ -1,5 +1,21 @@
 package com.g4dev.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ScriptController {
+	public void criarScript() {
+		//
+	}
+	public void deletarScript() {
+		//
+	}
+	public void buscarScript(String nome) {
+		//
+	}
+	
+	public void copiarScript() {
+		//
+	}
 
 }
--- src/main/java/com/g4dev/service/ScriptService.java
+++ src/main/java/com/g4dev/service/ScriptService.java
@@ -1,5 +1,24 @@
 package com.g4dev.service;
 
-public class ScriptService {
+import com.g4dev.model.ScriptModel;
 
+public class ScriptService {
+	
+	ScriptModel script;
+	
+	public void add(ScriptModel script) {
+		
+	}
+	
+	public void delete(ScriptModel script) {
+		
+	}
+	
+	public void search(ScriptModel script) {
+		
+	}
+	
+	public void atualizar(ScriptModel script) {
+		
+	}
 }

180773b8e177faf2ac156f1277849031a50e14f1
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-06-06 14:06:28 -0300

    Italo trabson

--- src/main/java/com/g4dev/controller/PaginaInicialController.java
+++ src/main/java/com/g4dev/controller/PaginaInicialController.java
@@ -1,9 +1,12 @@
 package com.g4dev.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+@Controller
 public class PaginaInicialController {
+
+	@RequestMapping("/")
 
 	public String paginaInicial() {
 		return "pagina-inicial";

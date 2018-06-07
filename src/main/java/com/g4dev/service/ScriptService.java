bf9a59abe70e529b2e547dc68272e1d219e651c2
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-06-07 00:28:52 -0300

    Adicionado mapeamento e comunicação com o repository.

--- src/main/java/com/g4dev/service/ScriptService.java
+++ src/main/java/com/g4dev/service/ScriptService.java
@@ -1,24 +1,40 @@
 package com.g4dev.service;
 
-import com.g4dev.model.ScriptModel;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import com.g4dev.model.Script;
+import com.g4dev.repository.ScriptRepository;
+import com.g4dev.util.G4devFileUtil;
 
+@Service
 public class ScriptService {
 	
-	ScriptModel script;
+	@Autowired
+	ScriptRepository scriptRepository;
 	
-	public void add(ScriptModel script) {
-		
+
+	public void salvarScript(Script script, MultipartFile imagem) {
+		// TODO Auto-generated method stub
+		String path = "src/main/resource/static/img/scripts/"+ script.getNome() +".png";
+		G4devFileUtil.salvarImagem(path, imagem);
+		script.setpath(path);
+		scriptRepository.save(script);
 	}
 	
-	public void delete(ScriptModel script) {
-		
+	public List<Script> listarPessoa(){
+		return scriptRepository.findAll();
+	}
+
+	public Script buscarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return scriptRepository.getOne(id);
+	}
+
+	public void excluirPorId(Long id) {
+		// TODO Auto-generated method stub
+		scriptRepository.delete(id);
 	}
 	
-	public void search(ScriptModel script) {
-		
-	}
-	
-	public void atualizar(ScriptModel script) {
-		
-	}
 }

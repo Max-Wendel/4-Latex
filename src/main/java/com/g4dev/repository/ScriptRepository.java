590591f7b6b3c53d04adb8f26aaaf775874060bc
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-06-07 00:29:53 -0300

    Criação do repository para conexão com o banco.

---
+++ src/main/java/com/g4dev/repository/ScriptRepository.java
@@ -0,0 +1,9 @@
+package com.g4dev.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.g4dev.model.Script;
+
+public interface ScriptRepository extends JpaRepository<Script, Long>{
+
+}

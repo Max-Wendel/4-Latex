5674287256ce40e2b372b66548c18e1b2d427b41
Ítalo <wowbrasiloficial@gmail.com>
2018-06-07 11:22:53 -0300

    Roteamento + "concerto" de templates

--- src/main/java/com/g4dev/controller/TemplateController.java
+++ src/main/java/com/g4dev/controller/TemplateController.java
@@ -1,7 +1,10 @@
 package com.g4dev.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("4Latex")
 public class TemplateController {
 	
 	@RequestMapping("/templates")

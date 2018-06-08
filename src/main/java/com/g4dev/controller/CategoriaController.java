b809914d04e90862476b45f9a558991581497757
Max-Wendel-UFC <maxwendelffaraujo@alu.ufc.br>
2018-06-07 22:44:17 -0300

    [MEGA-COMMIT] CRUD da classe Categoria.

--- src/main/java/com/g4dev/controller/CategoriaController.java
+++ src/main/java/com/g4dev/controller/CategoriaController.java
@@ -1,23 +1,57 @@
 package com.g4dev.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.g4dev.model.Categoria;
+import com.g4dev.service.CategoriaService;
 
 @Controller
 @RequestMapping("4Latex")
 public class CategoriaController {
+	
+	@Autowired
+	CategoriaService categoriaService;
+	
 	@RequestMapping("/categorias")
 	public String paginaCategorias() {
 		return "pagina-categorias";
 	}
 	
-	public void criarCat() {
-		//
+	@PostMapping("/salvarCategoria")
+	public ModelAndView salvar(Categoria categoria) {
+		categoriaService.salvarCategoria(categoria);
+		ModelAndView mv =new ModelAndView("redirect:/scripts");
+		return mv;
 	}
-	public void deletarCat() {
-		//
+	
+	@GetMapping("/listarCategoria")
+	public ModelAndView listarCategoria() {
+		List<Categoria> categorias = categoriaService.listarCategoria();
+		ModelAndView mv = new ModelAndView("redirect:/scripts");
+		mv.addObject("todasAsCategorias", categorias);
+		return mv;
 	}
-	public void buscarCategoria() {
-		//
+	
+	@RequestMapping("/atualizarCategoria/{id}")
+	public ModelAndView atualizarCategoria(@PathVariable Long id) {
+		Categoria categoria = categoriaService.buscarPorId(id);
+		ModelAndView mv = new ModelAndView("redirect:/scripts");
+		mv.addObject("categoria", categoria);
+		return mv;
+	}
+	
+	@RequestMapping("/excluir/{id}")
+	public ModelAndView ModelAndView(@PathVariable Long id) {
+		categoriaService.excluirPorId(id);
+		ModelAndView mv = new ModelAndView("redirect:/scripts");
+		return mv;
 	}
 }
--- src/main/java/com/g4dev/service/CategoriaService.java
+++ src/main/java/com/g4dev/service/CategoriaService.java
@@ -1,21 +1,40 @@
 package com.g4dev.service;
 
-import com.g4dev.model.Categoria;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.g4dev.model.Categoria;
+import com.g4dev.repository.CategoriaRepository;
+
+@Service
 public class CategoriaService {
 
-	Categoria categoria;
-	
-	public void add(Categoria categoria) {
-		
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	public void salvarCategoria(Categoria categoria) {
+		// TODO Auto-generated method stub
+		categoriaRepository.save(categoria);
 	}
-	
-	public void delete(Categoria categoria) {
-		
+
+	public List<Categoria> listarCategoria() {
+		// TODO Auto-generated method stub
+		return categoriaRepository.findAll();
 	}
-	
-	public void search(Categoria categoria) {
-		
+
+	public Categoria buscarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return categoriaRepository.findOne(id);
 	}
+
+	public void excluirPorId(Long id) {
+		// TODO Auto-generated method stub
+		categoriaRepository.delete(id);
+	}
+
+	
 	
 }
